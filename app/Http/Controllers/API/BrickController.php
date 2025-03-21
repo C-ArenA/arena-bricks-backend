fa6cb3a585f0e1c50ef0a566f338ab2f0cf044fa
C-ArenA_WSL <carlostata.ca@gmail.com>
2025-03-20 21:56:31 -0400

    feature: create API CRUD brick controller

---
+++ app/Http/Controllers/API/BrickController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\BrickResource;
+use App\Models\Brick;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class BrickController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): JsonResource
+    {
+        $bricks = Brick::all();
+        return BrickResource::collection($bricks);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Brick $brick)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Brick $brick)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Brick $brick)
+    {
+        //
+    }
+}
