b082b2c410ceeae295634bc158209f37563242ae
c-arena <carlostata.ca@gmail.com>
2025-03-23 10:38:40 -0400

    feature: add new routes to new brick functionality

--- routes/api/v1.php
+++ routes/api/v1.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\API\BrickController;
 use App\Http\Controllers\API\ProducedBrickController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/bricks', [BrickController::class, 'index']);
+Route::apiResource('bricks', BrickController::class);
 Route::get('/produced-bricks', [ProducedBrickController::class, 'index']);
 Route::post('/produced-bricks', [ProducedBrickController::class, 'store']);
 Route::delete('/produced-bricks/{brick}', [ProducedBrickController::class, 'destroy']);
